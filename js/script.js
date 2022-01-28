ffd6b16561a0ca63ac39e651ddbe75de5e6f782a
Bori-github <85009583+Bori-github@users.noreply.github.com>
2022-01-28 20:19:19 +0900

    :hammer: 다크모드 script 적용

--- js/script.js
+++ js/script.js
@@ -42,3 +42,42 @@ slider.addEventListener("touchend", end);
 mobileToggleBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
 });
+
+// dark mode
+const colorScheme = localStorage.getItem("color-scheme") || "light-mode";
+
+let darkModeState = false;
+
+const btnMode = document.querySelectorAll(".btn-mode");
+
+// MediaQueryList object
+const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Toggles the "dark-mode" class
+function toggleDarkMode(state) {
+  document.documentElement.classList.toggle("dark-mode", state);
+  darkModeState = state;
+}
+
+// Sets localStorage state
+function setDarkModeLocalStorage(state) {
+  localStorage.setItem("dark-mode", state);
+}
+
+// Initial setting
+toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+
+// Listen for changes in the OS settings.
+// Note: the arrow function shorthand works only in modern browsers,
+// for older browsers define the function using the function keyword.
+useDark.addListener((evt) => toggleDarkMode(evt.matches));
+
+// Toggles the "dark-mode" class on click and sets localStorage state
+btnMode.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    darkModeState = !darkModeState;
+
+    toggleDarkMode(darkModeState);
+    setDarkModeLocalStorage(darkModeState);
+  });
+});
